a5ca010fe3f3966b1705b920965d7c340cc36a9f
Vimal Raveendran <vimalraveendra@gmail.com>
2026-05-10 16:39:46 +0200

    feat: add parking grid header template and styles

---
+++ src/parking-grid/parking-grid.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parking-grid',
+  imports: [],
+  templateUrl: './parking-grid.component.html',
+  styleUrl: './parking-grid.component.scss'
+})
+export class ParkingGridComponent {
+
+}
