1ffc6b32142444328efbd49b28b64cad873c1afd
Vimal Raveendran <vimalraveendra@gmail.com>
2026-05-11 22:31:18 +0200

    feat:update parking grid template and styles with custom select

--- src/parking-grid/parking-grid.component.ts
+++ src/parking-grid/parking-grid.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CustomSelectComponent } from '../shared/components/custom-select/custom-select.component';
+import { PARKING_SLOTS, PARKING_TYPES } from '../shared/data/parking-categories';
 
 @Component({
   selector: 'app-parking-grid',
-  imports: [],
+  standalone:true,
+  imports: [CustomSelectComponent],
   templateUrl: './parking-grid.component.html',
   styleUrl: './parking-grid.component.scss'
 })
 export class ParkingGridComponent {
+  optionsData=PARKING_SLOTS;
+  optionsType=PARKING_TYPES;
 
 }
