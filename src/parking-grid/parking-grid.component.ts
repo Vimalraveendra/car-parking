8239af9f02b7c9ceddbb99209ea2f849b90b1e33
Vimal Raveendran <vimalraveendra@gmail.com>
2026-05-12 15:45:13 +0200

    feat:add parking floor rendering logic

--- src/parking-grid/parking-grid.component.ts
+++ src/parking-grid/parking-grid.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CustomSelectComponent } from '../shared/components/custom-select/custom-select.component';
 import { PARKING_SLOTS, PARKING_TYPES } from '../shared/data/parking-categories';
+import { ParkingService } from '../services/parking.service.ts.service';
 
 @Component({
   selector: 'app-parking-grid',
@@ -10,7 +11,19 @@ import { PARKING_SLOTS, PARKING_TYPES } from '../shared/data/parking-categories'
   styleUrl: './parking-grid.component.scss'
 })
 export class ParkingGridComponent {
+
+ private parkingService=inject(ParkingService)
+
+ parkingFloors=this.parkingService.floors;
+ activeParkingFloor=signal(0)
   optionsData=PARKING_SLOTS;
   optionsType=PARKING_TYPES;
+
+   setFloor(floor: number) { this.activeParkingFloor.set(floor); }
+
+    parkingFloorAvailable(floor: number): number {
+    return this.parkingFloors().find(f => f.number === floor)?.slots
+      .filter(s => s.status === 'available').length ?? 0;
+  }
 
 }
