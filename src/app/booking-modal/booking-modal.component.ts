d6ab292acdb98ddcd8078c83f50c9113e4e119cd
Vimal Raveendran <vimalraveendra@gmail.com>
2026-05-13 22:40:10 +0200

    feat:add vehicle types to parking categories and use in booking model component

--- src/app/booking-modal/booking-modal.component.ts
+++ src/app/booking-modal/booking-modal.component.ts
@@ -1,12 +1,14 @@
-import { Component, inject ,input, output} from '@angular/core';
+import { Component, inject ,input, output, signal} from '@angular/core';
 import { FormGroup,FormControl,Validators ,ReactiveFormsModule, MaxLengthValidator} from '@angular/forms';
 import { ParkingService } from '../../services/parking.service';
 import { Booking, ParkingSlot } from '../models/parking.model';
+import { CustomSelectComponent } from '../../shared/components/custom-select/custom-select.component';
+import { VEHICLE_TYPES } from '../../shared/data/parking-categories';
 
 @Component({
   selector: 'app-booking-modal',
   standalone:true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,CustomSelectComponent],
   templateUrl: './booking-modal.component.html',
   styleUrl: './booking-modal.component.scss'
 })
@@ -16,7 +18,8 @@ export class BookingModalComponent {
    submitting = false;
    closed =output<void>();
    booked=output<Booking>();
-
+  optionsType=VEHICLE_TYPES;
+  selectVehicleType=signal({label:'🚗 Car',value:'car'})
 
    bookingForm:FormGroup= new FormGroup({
     ownerName:new FormControl('',[Validators.required]),
